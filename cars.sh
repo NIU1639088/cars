2b02555c234b92b6cc620a6ff48db6a144ed0d0e
Jesús <jesus.caravaca@autonoma.cat>
2021-11-11 16:09:36 +0100

    first commit

---
+++ cars.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function OpcioError () {
+
+clear 
+echo: Error: Opcio no valida.
+
+sleep 3
+
+clear }
+
+menu () {
+
+echo '-----------------------------------'
+echo 'Base de Dades de cotxes'
+echo '1 Llistats de cotxes'
+echo '2 Cerca de cotxes'
+echo '3 Comprovem si ets realment un expert en cotxes'
+echo '4 Altres, baixes i modificacions de la base de dades'
+echo '0 Sortir'
+echo ' '
+
+}
